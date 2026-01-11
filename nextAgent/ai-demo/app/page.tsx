8e91d37eb41f92ecb7a93179fa38bd4912eecb88
Bob German <bgerman1@hotmail.com>
2026-01-11 04:34:06 +0000

    Improve landing page

--- nextAgent/ai-demo/app/page.tsx
+++ nextAgent/ai-demo/app/page.tsx
@@ -3,9 +3,16 @@
 export default function Home() {
 
   return (
-    <ul>
-      <li><a href="/tests/generateText">Generate Text</a></li>
-      <li><a href="/tests/streamText">Stream Text</a></li>
-    </ul>
+
+    <div className="max-w-2xl mx-auto p-6 min-h-screen bg-gray-50 text-gray-800">
+      <h1 className="text-3xl font-light text-gray-800 mb-8">
+        AI Demo
+      </h1>
+
+      <ul>
+        <li><a href="/tests/generateText">Generate Text</a></li>
+        <li><a href="/tests/streamText">Stream Text</a></li>
+      </ul>
+    </div>
   );
 }
