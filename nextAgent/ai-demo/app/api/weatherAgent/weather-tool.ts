63dbfd685c2cd4568194962d5737d693eb96d018
Bob German <bgerman1@hotmail.com>
2026-01-21 01:34:21 +0000

    Refactor weather agent to get lat/lon from AI model

--- nextAgent/ai-demo/app/api/weatherAgent/weather-tool.ts
+++ nextAgent/ai-demo/app/api/weatherAgent/weather-tool.ts
@@ -3,24 +3,40 @@ import { z } from 'zod';
 
 export const weatherTool = tool({
   description: 'Get the weather in a location',
-  inputSchema: z.object({ city: z.string() }),
-  async *execute({ city }: { city: string }) {
+  inputSchema: z.object({ city: z.string(), latitude: z.string(), longitude: z.string() }),
+  // async *execute({ city }: { city: string }) {
+  async *execute({ city, latitude, longitude }: {
+    city: string, latitude: string, longitude: string }) {
     yield { state: 'loading' as const };
 
     // TODO: Remove this debug log in production
-    console.log(`In weatherTool, fetching weather for ${city}`);
+    console.log(`In weatherTool, fetching weather for ${latitude}, ${longitude}`);
 
     // Add artificial delay of 5 seconds
     await new Promise(resolve => setTimeout(resolve, 2000));
 
-    const weatherOptions = ['sunny', 'cloudy', 'rainy', 'snowy', 'windy'];
-    const weather =
-      weatherOptions[Math.floor(Math.random() * weatherOptions.length)];
+    let weather = 'blizzard';             // default
+    let temperature = 30;                 // default
+    if (latitude === '40.7128') {         // New York
+      weather = 'scorching';
+      temperature = 95;
+    } else if (latitude === '34.0522') {  // Los Angeles
+      weather = 'foggy';
+      temperature = 65;
+    } else if (latitude === '41.8781') {  // Chicago
+      weather = 'tsunami';
+      temperature = 50;
+    } else if (latitude === '37.7749') {  //
+      weather = 'hurricane';
+      temperature = 55;
+    }
+    console.log(`Determined weather as ${weather} for city of ${city} at latitude ${latitude}`);
 
     yield {
       state: 'ready' as const,
-      temperature: 72,
+      temperature,
       weather,
+      city,
     };
   },
 });
