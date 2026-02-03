cdd17fdef306fc464ea0fc305718254ca5164a27
Bob German <bgerman1@hotmail.com>
2026-02-03 18:36:20 -0500

    Add instructions to the weather agent

--- nextAgent/ai-demo/app/api/weatherAgent/weather-agent.ts
+++ nextAgent/ai-demo/app/api/weatherAgent/weather-agent.ts
@@ -14,9 +14,18 @@ console.log('Initializing weather agent...');
 const model = modelProvider(process.env.LLM_MODEL_ID || "");
 console.log(`Using LLM model: ${process.env.LLM_MODEL_ID}`);
 
+const instructions = `
+ You are a helpful assistant and expert metiorologist.
+ Use the provided weather tool to answer user queries about the weather.
+ If the user asks about more than one location, be sure to use the weather tool for
+ each location separately.  If the user asks you to compare the weather
+ between multiple locations, present the comparison in a table.
+ Always provide the most accurate and up-to-date information available.
+`;
+
 export const weatherAgent = new ToolLoopAgent({
   model,
-  instructions: 'You are a helpful assistant.',
+  instructions,
   tools: {
     weather: weatherTool,
   },
