d776d5a5bacc169467de9bf3f5a80c1951e9fe40
Bob German <bgerman1@hotmail.com>
2026-02-05 20:59:25 -0500

    Extract embedding model creation and reuse it

--- nextAgent/ai-demo/app/api/generateEmbed/route.ts
+++ nextAgent/ai-demo/app/api/generateEmbed/route.ts
@@ -1,22 +1,12 @@
 import { embed } from "ai";
-import { LanguageModel } from 'ai';
-import { createOpenAICompatible } from '@ai-sdk/openai-compatible';
+import embeddimgModel from "../../models/embedModel";
 
 export async function POST(req: Request) {
 
   const { prompt } = await req.json();
 
-  const modelProvider = createOpenAICompatible({
-    name: 'lmstudio',
-    baseURL: process.env.LLM_BASE_URL || 'http://localhost:1234/v1',
-    supportsStructuredOutputs: true
-  });
-
-  const embeddingModel = modelProvider.embeddingModel(process.env.EMBEDDING_MODEL_ID || "")
-  console.log(`Using embedding model: ${process.env.EMBEDDING_MODEL_ID}`);
-
   const { embedding } = await embed({
-    model: embeddingModel,
+    model: embeddimgModel,
     value: prompt || "Hello, world!",
   });
 
